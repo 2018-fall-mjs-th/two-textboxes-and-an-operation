6c1f37cae48843740b8dba8832dd33b548ce341a
Ross Degrand <ross.degrand@fleetworthy.com>
2018-10-11 08:35:27 -0500

    Installed Angular and component

---
+++ src/app/tjs-number-addition/tjs-number-addition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tjs-number-addition',
+  templateUrl: './tjs-number-addition.component.html',
+  styleUrls: ['./tjs-number-addition.component.css']
+})
+export class TjsNumberAdditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
