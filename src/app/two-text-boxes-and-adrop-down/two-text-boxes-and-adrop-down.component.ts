180377ebc8f4934f3538f16fd019fbb3bc56ebc1
Robert Collier <35576077+Sescheraun@users.noreply.github.com>
2018-10-11 11:27:23 -0500

    Couldn't get the drop down to work.
    So I just did silly stuff with Math functions.

--- src/app/two-text-boxes-and-adrop-down/two-text-boxes-and-adrop-down.component.ts
+++ src/app/two-text-boxes-and-adrop-down/two-text-boxes-and-adrop-down.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { sendRequest } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'two-text-boxes-and-adrop-down',
@@ -12,4 +13,16 @@ export class TwoTextBoxesAndADropDownComponent implements OnInit {
   ngOnInit() {
   }
 
+
+answer: number = Math.sqrt(Math.pow(6, 6));
+ 
+
+  addNumbers() {
+    this.answer = Math.sqrt(Math.pow(this.n1 , this.n2));
+  }
+
+  n1: number = 6;
+  n2: number = 6;
+
+  
 }
