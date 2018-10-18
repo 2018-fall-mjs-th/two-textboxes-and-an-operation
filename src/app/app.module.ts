0440709341713f46d8bf360341d8d49ffd4dad7d
Paul Cotts <pdcotts@users.noreply.github.com>
2018-10-18 18:22:08 -0500

    installed ng-bootstrap and set it up.
    npm install --save @ng-bootstrap/ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,8 @@ import { RdSearchStringComponent } from './rd-search-string/rd-search-string.com
 import { TjsStringConcatenationComponent } from './tjs-string-concatenation/tjs-string-concatenation.component';
 import { EllisTakeTwoEqualityComponent } from './ellis-take-two-equality/ellis-take-two-equality.component';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,7 +53,8 @@ import { EllisTakeTwoEqualityComponent } from './ellis-take-two-equality/ellis-t
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [MatrixService],
   bootstrap: [AppComponent]
